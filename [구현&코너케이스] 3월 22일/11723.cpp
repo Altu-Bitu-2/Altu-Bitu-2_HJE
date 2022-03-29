a1f3988c65a092f7105df4456b9efa9e0bcbacdf
jaeeunHwang <98397375+jaeeunHwang@users.noreply.github.com>
2022-03-29 21:32:29 +0900

    Update 11723.cpp

--- [구현&코너케이스] 3월 22일/11723.cpp
+++ [구현&코너케이스] 3월 22일/11723.cpp
@@ -1,75 +1,76 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-void add(vector<int> s) {
+void add(vector<bool> &s) {
     int x;
     cin >> x;
-    if (s[x] != 0) {
-        s[x] = 1;
-    }
+    s[x] = true;
 }
 
-void remove(vector<int> s) {
+void remove(vector<bool> &s) {
     int x;
     cin >> x;
-    if (s[x] == 1) {
-        s[x] = 0;
-    }
+    s[x] = false;
 }
 
-void check(vector<int> s) {
+void check(vector<bool> &s) {
     int x;
     cin >> x;
-    if (s[x] != 0) {
-        cout << 1 << '\n';
-    } else{
-        cout << 0 << '\n';
-    }
+    cout << s[x] << '\n';
 }
 
-void toggle(vector<int> s) {
+void toggle(vector<bool> &s) {
     int x;
     cin >> x;
-    if (s[x] != 0) {
-        s[x] = 0;
-    } else {
-        s[x] = 1;
-    }
+    s[x] = !s[x];
 }
 
-void all(vector<int> s) {
-    for (int i = 0; i < 20; i++){
-        s[i] = 1;
-    }
+void all(vector<bool> &s) {
+    s.assign(21, true);
 }
 
-void empty(vector<int> s) {
-    for (int i = 0; i < 20; i++){
-        s[i] = 0;
-    }
+void empty(vector<bool> &s) {
+    s.assign(21, false);
 }
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int m;
     string input;
     cin >> m;
-    vector<int> s(20, 0);
+    vector<bool> s(21, false);
+
     while (m--) {
         cin >> input;
-        if (input == "add")
-                add(s);
-        if (input == "remove")
-                remove(s);
-        if (input == "check")
-                check(s);
-        if(input == "toggle")
-                toggle(s);
-        if(input == "all")
-                all(s);
-        if(input == "empty")
-                empty(s);
+        if (input == "add"){
+            add(s);
+            continue;
+        }
+        if (input == "remove"){
+            remove(s);
+            continue;
+        }
+        if (input == "check"){
+            check(s);
+            continue;
+        }
+        if(input == "toggle"){
+            toggle(s);
+            continue;
+        }
+        if(input == "all"){
+            all(s);
+            continue;
+        }
+        if(input == "empty"){
+            empty(s);
+            continue;
+        }
     }
     return 0;
 }
